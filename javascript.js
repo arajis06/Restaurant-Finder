58140e029da01d4d214c9bb0c3d0cde76c660ec2
n.mikey.truong <n.mikey.truong@gmail.com>
2019-06-10 22:05:49 -0400

    added the firebase config to javascript

---
+++ javascript.js
@@ -0,0 +1,14 @@
+
+// if anyone needs access to the firebase account the email is "acnproject01@gmail.com" pw: gitcheckout01
+// made that gmail for this project 
+var firebaseConfig = {
+    apiKey: "AIzaSyCF_6CAISGlBiTGlO0_YDuZ8gM9-4yRoeo",
+    authDomain: "acn-bootcamp-project-01.firebaseapp.com",
+    databaseURL: "https://acn-bootcamp-project-01.firebaseio.com",
+    projectId: "acn-bootcamp-project-01",
+    storageBucket: "acn-bootcamp-project-01.appspot.com",
+    messagingSenderId: "532356596825",
+    appId: "1:532356596825:web:87d4b837e270adec"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
