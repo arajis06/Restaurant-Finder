2542f886092bb0f6dd8966fd48c439b023e9e429
n.mikey.truong <n.mikey.truong@gmail.com>
2019-06-13 14:06:16 -0400

    table added and quick fix to javascript link

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -16,63 +16,58 @@ $("button").on("click", function(event) {
     })
     // After data comes back from the request
     .then(function(response) {
-    console.log(queryURLgeo);
-    console.log(response);
+        console.log(queryURLgeo);
+        console.log(response);
 
-    // storing the data from the AJAX request in the results variable
-    var results = response.Response.View[0].Result[0].Location.NavigationPosition[0]
-    console.log(results.Latitude)
-    console.log(results.Longitude)
+        // storing the data from the AJAX request in the results variable
+        var results = response.Response.View[0].Result[0].Location.NavigationPosition[0]
+        console.log(results.Latitude)
+        console.log(results.Longitude)
 
     
 
-    // URL and key for Zomato API
-    // !!!!! URL may need changed to match search criteria
-    var queryURLzomato = "https://developers.zomato.com/api/v2.1/geocode?lat=" + results.Latitude + "&lon=" + results.Longitude + "&apikey=ae4575e5962528d6786d05daee388045"
-    ;
+        // URL and key for Zomato API
+        // !!!!! URL may need changed to match search criteria
+        var queryURLzomato = "https://developers.zomato.com/api/v2.1/geocode?lat=" + results.Latitude + "&lon=" + results.Longitude + "&apikey=ae4575e5962528d6786d05daee388045"
+        ;
 
-    $.ajax({
-        url: queryURLzomato,
-        method: "GET"
-    })
-    // After data comes back from the request
-    .then(function(response2) {
-    console.log(response2);
+        $.ajax({
+            url: queryURLzomato,
+            method: "GET"
+        })
+        // After data comes back from the request
+        .then(function(response2) {
+            console.log(response2);
 
-    var results2 = response2.nearby_restaurants
-    console.log(results2)
+            var results2 = response2.nearby_restaurants
+            console.log(results2)
+
+        
+                // CURRENTLY IS NOT WORKING
+                for (var i = 0; i < results2.length; i++) {
+                    console.log(results2[i].restaurant.name)
+                    $("#name").append(results2[i].restaurant.name)
+                    console.log(results2[i].restaurant.location.address)
+                    $("#address").append(results2[i].restaurant.location.address)
+                    console.log(results2[i].restaurant.cuisines)
+                    $("#cuisines").append(results2[i].restaurant.cuisines)
+                    console.log(results2[i].restaurant.menu_url)
+                    $("#menu-url").append(results2[i].restaurant.menu_url)
+                    console.log(results2[i].restaurant.url)
+                    $("#restaurant-url").append(results2[i].restaurant.url)
+                    console.log(results2[i].restaurant.user_rating.aggregate_rating)
+                    $("#rating").append(results2[i].restaurant.user_rating.aggregate_rating)
 
 
-    // CURRENTLY IS NOT WORKING
-        for (var i = 0; i < results2.length; i++) {
-            console.log(results2[i].restaurant.name)
-            $("#name").append(results2[i].restaurant.name)
-            console.log(results2[i].restaurant.location.address)
-            $("#address").append(results2[i].restaurant.location.address)
-            console.log(results2[i].restaurant.cuisines)
-            $("#cuisines").append(results2[i].restaurant.cuisines)
-            console.log(results2[i].restaurant.menu_url)
-            $("#menu-url").append(results2[i].restaurant.menu_url)
-            console.log(results2[i].restaurant.url)
-            $("#restaurant-url").append(results2[i].restaurant.url)
-            console.log(results2[i].restaurant.user_rating.aggregate_rating)
-            $("#rating").append(results2[i].restaurant.user_rating.aggregate_rating)
-        }
 
+
+                   
+                }
+
+        })
     })
 })
-})
 
-// if anyone needs access to the firebase account the email is "acnproject01@gmail.com" pw: gitcheckout01
-// made that gmail for this project 
-var firebaseConfig = {
-    apiKey: "AIzaSyCF_6CAISGlBiTGlO0_YDuZ8gM9-4yRoeo",
-    authDomain: "acn-bootcamp-project-01.firebaseapp.com",
-    databaseURL: "https://acn-bootcamp-project-01.firebaseio.com",
-    projectId: "acn-bootcamp-project-01",
-    storageBucket: "acn-bootcamp-project-01.appspot.com",
-    messagingSenderId: "532356596825",
-    appId: "1:532356596825:web:87d4b837e270adec"
-};
+
 
 
